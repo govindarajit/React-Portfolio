bb10a169d37271561b7cf30ddfa1eb1aafe162bf
govindarajmrp@gmail.com <govindarajmrp@gmail.com>
2019-12-31 21:21:20 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Govindaraj",
+    "role": "Full stack Developer(MERN/MEAN/MEVN)",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to built full stack application and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/govindaraj-m-ab7783126/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"#",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"#",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I graduated from Adhiyamaan college Engineering of B.Tech in Information Technology. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"#",
+    "education":[
+      {
+        "UniversityName":"Adhiyamaan College of Engineering(autonomous),",
+        "specialization":"B.Tech(IT)",
+        "MonthOfPassing":"Apr",
+        "YearOfPassing":"2019",
+        "Achievements":"Some Achievements"
+      }, 
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2019",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"MERN/MEAN/MEVN stack development"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"Reactjs"
+      },{
+        "skillname":"AngularJS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
